49ba0a7692998a1fd3e2fa0525b3b629de07d693
yjjjwww <yjjjwww@naver.com>
2023-06-25 03:03:46 +0900

    add : test controller

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+  @GetMapping
+  public ResponseEntity<String> getArticleByPageable() {
+    return ResponseEntity.ok().body("Hello!");
+  }
+}
